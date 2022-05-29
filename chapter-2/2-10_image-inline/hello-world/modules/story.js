baa62ea05b54080846f0a7e4b39af9f57fb24d16
Tomoaki Oshima <mindboardapps@gmail.com>
2022-05-29 11:40:11 +0900

    Fix story module.

--- chapter-2/2-10_image-inline/hello-world/modules/story.js
+++ chapter-2/2-10_image-inline/hello-world/modules/story.js
@@ -1,11 +1,13 @@
 const story = {};
 
 story.getInsertionPoint = (textFrame)=>{
-    return textFrame.parentStory.insertionPoints[-1];
+    const s = textFrame.parentStory;
+    return s.insertionPoints[-1];
 };
 
 story.addBr = (textFrame)=>{
-	story.getInsertionPoint(textFrame).contents = '\r';
+    const ip = story.getInsertionPoint(textFrame);
+    ip.contents = '\r';
 };
 
 module.exports = story;
